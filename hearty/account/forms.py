c28001d3c303030d497b99c07b55ae3e4455573d
chimchimster <jorddyishere@gmail.com>
2023-07-11 17:56:54 +0600

    add settings for changing email

--- hearty/account/forms.py
+++ hearty/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User
+from phone_field import PhoneField
 from datetime import datetime
 
 
@@ -9,13 +10,18 @@ class RegisterUserForm(forms.ModelForm):
 
     BIRTH_YEAR_CHOICES = range(1950, start_year)
 
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     bdate = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
 
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'phone_number']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -28,7 +34,16 @@ class LoginUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'password']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-input'})
+        }
 
 
+class ChangeEmailForm(forms.Form):
+    email1 = forms.CharField(label='Новая почта', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    email2 = forms.CharField(label='Подтверждение новой почты', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
 
--- hearty/account/urls.py
+++ hearty/account/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+    path('<int:pk>/', views.AccountDetailView.as_view(), name='account-detail'),
+    path('change-email/', views.ChangeEmailView.as_view(), name='change-email'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('login/', views.LoginUser.as_view(), name='login'),
 ]

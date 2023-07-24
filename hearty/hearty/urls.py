aaf6de6eab34bf19eb2a0ec0dd4f0e2d8047a9f3
chimchimster <jorddyishere@gmail.com>
2023-07-24 14:28:08 +0600

    started chat consumers + js

--- hearty/hearty/urls.py
+++ hearty/hearty/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
     path('profile/', include('profiles.urls', namespace='profile')),
+    path('messaging/', include('messaging.urls', namespace='messaging')),
     path('ws/', include(routing.websocket_urlpatterns)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
